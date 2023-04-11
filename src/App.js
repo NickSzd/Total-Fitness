18965b60ebda4f9299ea985d5c15ec85141ea625
NickSzd <nsszwed@gmail.com>
2023-04-11 14:44:51 -0700

    began editing home page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box, Container, Paper, Typography } from "@mui/material";
+import "./App.css";
 
-function App() {
+const lst = ["item1", "item2", "item3"];
+const App = () => {
+  const title = "Total-Fitness";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container sx={{ bgcolor: "lightblue", height: "100vh" }}>
+      <Typography variant="h1">Total Fitness</Typography>
+      <Box sx={{ display: "flex" }}>
+        {lst.map((service) => (
+          <Paper elevation={3}>
+            <Typography>{service}</Typography>
+          </Paper>
+        ))}
+      </Box>
+    </Container>
   );
-}
+  //const isUserLogedIn = false; // render login button
+  // return (
+  //   <div className="App">
+  //     <h1>Hello</h1>
+  //   </div>
+  // );
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { createTheme, ThemeProvider } from "@mui/material";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const theme = createTheme({
+  palette: {
+    primary: { main: "#013e87" },
+    secondary: { main: "#2e74c9" },
+  },
+  typography: {
+    h1: {
+      fontSize: "3rem",
+      fontWeight: 600,
+    },
+  },
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
