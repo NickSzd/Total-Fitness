a64ab6c6241c59e55a0c692436896a10ee63470e
NickSzd <nsszwed@gmail.com>
2023-04-20 13:30:18 -0700

    Menu button now working

--- src/App.js
+++ src/App.js
@@ -7,33 +7,152 @@ import FitnessHome from "./pages/FitnessHome";
 import NutritionHome from "./pages/user_pages/NutritionHome";
 import UserHome from "./pages/user_pages/UserHome";
 import UserProfile from "./pages/user_pages/UserProfile";
+import Login from "./containers/Login";
 import {
   BrowserRouter as Router,
   Routes,
   Route,
-  Redirect,
-  useNavigate,
+
+  // Redirect,
+  // useNavigate,
 } from "react-router-dom";
-import { render } from "react-dom";
-import { ReactDOM } from "react";
+// import { render } from "react-dom";
+// import { ReactDOM } from "react";
+import {
+  IconButton,
+  Button,
+  Typography,
+  Box,
+  AppBar,
+  Toolbar,
+  Menu,
+  MenuItem,
+} from "@mui/material";
+import { useState } from "react";
+
 const lst = ["item1", "item2", "item3"];
 ///home/nick/Documents/CSE115/total-fitness/src/pages/about.js
 
 function App() {
+  const [anchor, setAnchor] = useState(null);
+  const menuOptions = [
+    "home",
+    "about",
+    "userHome",
+    "nutritionHome",
+    "fitnessHome",
+    "userProfile",
+  ];
+  const [selected, setSelected] = useState(-1);
+
+  //Lock the menu open when clicked
+  const openMenu = (event) => {
+    setAnchor(event.currentTarget);
+  };
+
+  //Closes Menu on Click
+  const closeMenu = () => {
+    setAnchor(null);
+  };
+
+  // On Selecting an item from the menu
+  const onMenuItemClick = (event, index) => {
+    setAnchor(null);
+    setSelected(index);
+  };
+
+  const openLogin = () => {
+    console.log("login call");
+  };
+
+  // Allows for page redirects
+  // let navigate = useNavigate();
+  // const routeChange = (index) => {
+  //   let path = `./About`;
+  //   navigate(path);
+  // };
+
   //const navigate = useNavigate();
   return (
-    <Router>
-      <div>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/About" element={<About />} />
-          <Route path="/FitnessHome" element={<FitnessHome />} />
-          <Route path="/NutritionHome" element={<NutritionHome />} />
-          <Route path="/UserHome" element={<UserHome />} />
-          <Route path="/UserProfile" element={<UserProfile />} />
-        </Routes>
+    <div
+      className="home"
+      style={{
+        color: "black",
+      }}
+    >
+      <Box sx={{ flexgrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              onClick={openMenu}
+              size="large"
+              edge="start"
+              aria-label="menu"
+              color="white"
+              sx={{ mr: 2 }}
+            >
+              Menu
+            </IconButton>
+            <Menu
+              open={Boolean(anchor)}
+              anchorEl={anchor}
+              onClose={closeMenu}
+              keepMounted
+            >
+              {menuOptions.map((item, index) => (
+                <MenuItem
+                  key={index}
+                  onClick={(event) => {
+                    document.location.href = "/" + item;
+                    console.log(item);
+                  }}
+                  selected={index === selected}
+                >
+                  {item}
+                </MenuItem>
+              ))}
+            </Menu>
+            <Typography variant="h4" component="div" sx={{ flexGrow: 1 }}>
+              Total Fitness
+            </Typography>
+            <Button id="loginButton" color="inherit">
+              Login
+            </Button>
+            {/* <button
+              id="loginButton"
+              class="float-left submit-button"
+              color="inherit"
+            >
+              Login
+            </button> */}
+            <script type="text/javascript">
+              //console.log("Button Clicked");
+              document.getElementById("loginButton").onclick =
+              console.log("button click event");
+              {/* {
+                //(location.href = "/Login")
+              } */}
+              ;
+            </script>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <div className="routing">
+        <Router>
+          <div>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/About" element={<About />} />
+              <Route path="/FitnessHome" element={<FitnessHome />} />
+              <Route path="/NutritionHome" element={<NutritionHome />} />
+              <Route path="/UserHome" element={<UserHome />} />
+              <Route path="/UserProfile" element={<UserProfile />} />
+              <Route path="/Login" element={<Login />} />
+            </Routes>
+          </div>
+        </Router>
       </div>
-    </Router>
+    </div>
   );
   //return <Home />;
   //return <Login />;
@@ -51,5 +170,20 @@ function App() {
   //   </Container>
   // );
 }
+
+/*
+<Router>
+          <div>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/About" element={<About />} />
+              <Route path="/FitnessHome" element={<FitnessHome />} />
+              <Route path="/NutritionHome" element={<NutritionHome />} />
+              <Route path="/UserHome" element={<UserHome />} />
+              <Route path="/UserProfile" element={<UserProfile />} />
+            </Routes>
+          </div>
+        </Router>
+*/
 
 export default App;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,27 +10,6 @@ Reqs
 
 import React from "react";
 import "../App.css";
-import {
-  IconButton,
-  Button,
-  Typography,
-  Box,
-  AppBar,
-  Toolbar,
-  Menu,
-  MenuItem,
-} from "@mui/material";
-import { useState } from "react";
-// import {
-//   BrowserRouter as Router,
-//   Switch,
-//   Route,
-//   Redirect,
-// } from "react-router-dom";
-import { Link, useNavigate } from "react-router-dom";
-
-// import { Menu } from "@mui/icons-material";
-// import Menu from "@mui/material";
 
 function Home() {
   // Sets Up the Menu Button
@@ -42,101 +21,23 @@ function Home() {
     Landing Page
   Eventually this will be for contact, info, and about
   */
-  const [anchor, setAnchor] = useState(null);
-  const menuOptions = [
-    "home",
-    "about",
-    "userHome",
-    "nutritionHome",
-    "fitnessHome",
-    "userProfile",
-  ];
-  const [selected, setSelected] = useState(-1);
-
-  //Lock the menu open when clicked
-  const openMenu = (event) => {
-    setAnchor(event.currentTarget);
-  };
-
-  //Closes Menu on Click
-  const closeMenu = () => {
-    setAnchor(null);
-  };
-
-  // On Selecting an item from the menu
-  const onMenuItemClick = (event, index) => {
-    setAnchor(null);
-    setSelected(index);
-  };
-
-  // Allows for page redirects
-  // let navigate = useNavigate();
-  // const routeChange = (index) => {
-  //   let path = `./About`;
-  //   navigate(path);
-  // };
 
   return (
-    <div className="home">
-      <Box sx={{ flexgrow: 1 }}>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton
-              onClick={openMenu}
-              size="large"
-              edge="start"
-              aria-label="menu"
-              color="white"
-              sx={{ mr: 2 }}
-            >
-              Menu
-            </IconButton>
-            <Menu
-              open={Boolean(anchor)}
-              anchorEl={anchor}
-              onClose={closeMenu}
-              keepMounted
-            >
-              {menuOptions.map((option, index) => (
-                <MenuItem
-                  key={index}
-                  onClick={(event) => {
-                    console.log(index);
-                  }}
-                  selected={index === selected}
-                >
-                  {option}
-                </MenuItem>
-              ))}
-            </Menu>
-            <Typography variant="h4" component="div" sx={{ flexGrow: 1 }}>
-              Total Fitness
-            </Typography>
-            <Button id="loginButton" color="inherit">
-              Login
-            </Button>
-          </Toolbar>
-        </AppBar>
-      </Box>
-      <div
-        className="title"
-        style={{
-          color: "blue",
-          width: "fit-content",
-          margin: "auto",
-        }}
-      >
-        <h1>Total Fitness</h1>
-        <strong style={{ color: "black" }}>
-          {" "}
-          The Complete Fitness Application
-        </strong>
-        <h1>
-          Right now this is super gheto just type /pagename after local host to
-          whatever page you are working on
-        </h1>
-      </div>
-      <div className="landingBody"></div>
+    <div
+      className="main"
+      style={{
+        color: "blue",
+        width: "fit-content",
+        margin: "auto",
+      }}
+    >
+      <h1>Total Fitness</h1>
+      <strong style={{ color: "black" }}>
+        {" "}
+        The Complete Fitness Application
+      </strong>
+      <h1>Access the page you are working on with the menu</h1>
+      <div className="body"></div>
     </div>
   );
 }
