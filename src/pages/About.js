a549f4d34d14939e43da3a48ea53ccf46f8200a0
Vaibhav Honakere <vhonaker@ucsc.edu>
2023-04-25 12:33:13 -0700

    a few changes to the home and about

--- src/pages/About.js
+++ src/pages/About.js
@@ -13,6 +13,19 @@ function About() {
   return (
     <div>
       <h1>About Page</h1>
+      <div class="main" id="about">
+      <div class="main__container">
+        <div class="main__img--container">
+          <div class="main__img--card"><i class="fas fa-layer-group"></i></div>
+        </div>
+        <div class="main__content">
+          <h1>What do we do?</h1>
+          <h2>We help people reach their fitness goal</h2>
+          <p>Login and start tracking your daily nutrition and workout plan for greater rewards for yourself.</p>
+          <button class="main__btn"><a href="#">Sign up</a></button>
+        </div>
+      </div>
+    </div>
     </div>
   );
 }
