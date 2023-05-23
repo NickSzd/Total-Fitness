29e98c1f639796b72b3f7735f2a12832ec68ada0
phudis rangsisuriyachai <prangsis@ucsc.edu>
2023-05-23 00:45:21 -0700

    Please take a look on the webpage and API Hiding!

--- src/pages/Summary.js
+++ src/pages/Summary.js
@@ -1,18 +1,9 @@
+import React from "react";
+
+
 function Summary() {
   return (
     <div className="main" style={{ color: "white" }}>
-      <h1>Total Fitness</h1>
-      <strong> The Complete Fitness Application</strong>
-      <h1>Access the page you are working on with the menu</h1>
-      <div className="body">
-        When You Start Working on Your page do the following
-        <ol>
-          <li>Pull the latest Version from git</li>
-          <li>Create a branch titled with the page you are working on</li>
-          <li>Make sure you are on that branch before you start working</li>
-          <li>When you push make sure to add someone to review your request</li>
-        </ol>
-      </div>
       <div class="hero" id="home">
         <div class="hero__container">
           <h1 class="hero__heading">
@@ -24,7 +15,28 @@ function Summary() {
           </button>
         </div>
       </div>
+      <div className="main" id="about">
+        <div className="main__container">
+          <div className="main__img--container">
+            <div className="main__img--card">
+              <i className="fas fa-layer-group"></i>
+            </div>
+          </div>
+          <div className="main__content">
+            <h1>What do we do?</h1>
+            <h2>We help people reach their fitness goal</h2>
+            <p>
+              Login and start tracking your daily nutrition and workout plan for
+              greater rewards for yourself.
+            </p>
+            <button className="main__btn">
+              <a href="/Register">Sign up</a>
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
+
   );
 }
 export default Summary;
