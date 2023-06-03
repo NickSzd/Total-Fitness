8e2ab9719933a0c1921a38d223bc4a9aa07785bc
Julio Vasquez-Bonilla <juanvasq@ucsc.edu>
2023-06-02 18:44:42 -0700

    updated

--- src/pages/user_pages/components/Questionaire/start.js
+++ src/pages/user_pages/components/Questionaire/start.js
@@ -1,22 +1,40 @@
 import Button from "@mui/material/Button";
 import MidGame from "./mid";
 import EndGame from "./end";
-import {useState} from "react";
-import "./gameP1.css"
-import "./gameP2.css"
+import { useState } from "react";
+import "./gameP1.css";
+import "./gameP2.css";
 
-const BegGame = (handleNext) => {
-  return(
-  <>
-    <div class="container">
-      <div id="home" class="flex-center flex-column">
-        <h1>Registration Information</h1>
-        <Button onClick={handleNext}>Continue</Button>
+const BegGame = ({ handleNext }) => {
+  return (
+    <>
+      <div class="container">
+        <div id="home" class="flex-center flex-column">
+          <h1>Registration Information</h1>
+          <Button
+            onClick={() => {
+              handleNext();
+            }}
+          >
+            Continue
+          </Button>
+        </div>
       </div>
-    </div>
-  </>
+    </>
   );
 };
+
+function getContent(step, handleNext) {
+  // eslint-disable-next-line default-case
+  switch (step) {
+    case 0:
+      return <BegGame handleNext={handleNext} />;
+    case 1:
+      return <MidGame handleNext={handleNext} />;
+    case 2:
+      return <EndGame handleNext={handleNext} />;
+  }
+}
 function Start() {
   const [step, setStep] = useState(0);
 
@@ -26,21 +44,10 @@ function Start() {
       handleClose();
     }
   };
-  function getContent(step) {
-    // eslint-disable-next-line default-case
-    switch (step) {
-      case 0:
-        console.log("printMEEE");
-        return <BegGame handleNext={handleNext}/>;
-      case 1:
-        return <MidGame handleNext={handleNext}/>;
-      case 2:
-        return <EndGame handleNext={handleNext}/>;
-    }
-  }
+
   const handleClose = () => {
-      setStep(0);
+    setStep(0);
   };
-  return <>{getContent(handleNext)}</>;
+  return <>{getContent(step, handleNext)}</>;
 }
 export default Start;
